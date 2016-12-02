a9f15ba9672efbc6573e827bca832842ce4da885
David Salgado <david.salgado@digital.justice.gov.uk>
2016-12-02 13:43:01 +0000

    ignore js errors

--- features/env.rb
+++ features/env.rb
@@ -8,3 +8,8 @@ Capybara.run_server = false
 Capybara.default_driver = :poltergeist
 Capybara.javascript_driver = :poltergeist
 
+options = {js_errors: false}
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, options)
+end
+
