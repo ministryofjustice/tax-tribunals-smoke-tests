64c20e6632c48e354ac60cc16d8db4295c7a4fd0
David Salgado <david.salgado@digital.justice.gov.uk>
2016-12-12 15:57:13 +0000

    update redirect test wrt. payment changes
    There is now more than one way to pay, so this test needed updating

--- mechanize-scripts/redirect-to-govuk-pay.rb
+++ mechanize-scripts/redirect-to-govuk-pay.rb
@@ -2,6 +2,7 @@
 
 require 'bundler/setup'
 require 'mechanize'
+require 'awesome_print'
 
 # Test that searching for a case with an outstanding fee and then clicking 'Pay now'
 # results in a credit card payment form being shown to the user.
@@ -14,12 +15,18 @@ def main
   a.get(ENV.fetch('FEE_PAYMENT_URL')) do |page|
     search_page  = a.click(page.link_with(text: /Start now/))
     results_page = search_for_case(search_page, ENV.fetch('CASE_WITH_FEE_REF'), ENV.fetch('CASE_WITH_FEE_CONF_CODE'))
-    payment_page = click_link(a, results_page, 'Pay now')
+    payment_page = choose_credit_card(a, results_page)
 
     test_payment_form payment_page
   end
 
   puts_green "    Passed."
+end
+
+def choose_credit_card(a, page)
+  page.form_with(action: /payments/) do |f|
+    f.radiobutton_with(id: 'payment_method_card').check
+  end.click_button
 end
 
 def search_for_case(page, case_reference, confirmation_code)
@@ -35,7 +42,7 @@ def click_link(a, page, link_text)
 end
 
 def test_payment_form(page)
-  page.form_with(action: '/card_details') do |f|
+  page.form_with(name: 'cardDetails') do |f|
     field_names = f.fields.map(&:name)
     %w(cardNo expiryMonth cvc).map {|name|
       raise("Missing field #{name} in credit card payment details form") unless field_names.include?(name)
